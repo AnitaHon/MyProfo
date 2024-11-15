f881b0b4788433e2443ec34d7b09f3c8bd89a8a4
AnitaHon <Ahon2@myseneca.ca>
2024-11-15 14:45:13 -0500

    hi

---
+++ main.js
@@ -0,0 +1,12 @@
+
+/*---------------------------------------------- 1 -----------------------------------------------*/
+import Glide from '@glidejs/glide';
+import anime from 'animejs';
+
+new Glide('.glide').mount();
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Glide('.glide').mount();
+});
+
+/*----------------------------------------------  -----------------------------------------------*/
